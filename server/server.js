8b780fdc0327b18fa7031c6eaad6ffd429dc80bd
Patrick Hammon <phammon@Patricks-MBP.home>
2018-01-22 21:26:04 -0500

    first try using websockets

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const path = require('path');
 //now we don't have to go up a directory
 const publicPath = path.join(__dirname + '/../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-console.log(publicPath);
-
-var app = express();
- //tells heroku to use whatever port it needs or if were running locally just use 3000
 const port = process.env.PORT || 3000
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+ //tells heroku to use whatever port it needs or if were running locally just use 3000
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect', () => {
+        console.log('user is no longer connected');
+    });
+})
+server.listen(port, () => {
     console.log(`started listening on ${port}`);
 })
 
